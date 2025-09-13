34e17cce7d274c57041950c2c9be6a5a6487369b
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2025-09-13 17:22:33 +0800

    20250913

--- src/org/rmj/mis/util/Create3C.java
+++ src/org/rmj/mis/util/Create3C.java
@@ -26,8 +26,8 @@ public class Create3C {
         
         Create3CLeads create = new Create3CLeads(instance);
         
-        create.setDateFrom("2025-05-03");
-        create.setDateThru("2025-06-02");
+        create.setDateFrom("2025-09-03");
+        create.setDateThru("2025-10-02");
         
         System.out.println(instance.getBranchCode());
         if (!create.Create()){
